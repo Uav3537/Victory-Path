0ad80f7b8894702049cef11073e14492e09912fa
Uav3537 <uav3537@gmail.com>
2025-09-20 12:01:18 +0900

    Auto commit via push.json

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(cors({ origin: '*' }))
 app.use(express.json())
 
 app.listen(PORT, () => {
