ee513946c671dd6512d442ae41b71c8c5ff81969
Uav3537 <uav3537@gmail.com>
2025-09-20 11:27:56 +0900

    Fix index.js for Render deployment

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.listen(PORT, () => {
+  console.log(`✅Server running on port ${PORT}`);
+})
+
+app.all("*", async(req, res) => {
+    console.log(req.body)
+    if(req.path == "/register") {
+        console.log(req.body)
+        
+    }
+    res.json({code: 0})
+})
